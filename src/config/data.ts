67be52a1e829a71af6b8b9cb2a3ea8df1d76d880
Likith Reddy S <likithreddy@Mac-1.lan>
2026-08-20 02:41:37 +0530

    added cv

--- src/config/data.ts
+++ src/config/data.ts
@@ -14,7 +14,7 @@ export const personalInfo = {
 export const cvConfig = {
   enabledInHero: false,
   enabledInNavbar: true,
-  url: "https://raw.githubusercontent.com/LikithReddy07/portfolio/refs/heads/main/index.html", // replace with your actual CV PDF URL
+  url: "https://github.com/LikithReddy07/portfolio/blob/0868c3cf567adb733ded819fede34121b3ce82b3/public/Likith_CV_V2.pdf", // replace with your actual CV PDF URL
   forceDownload: false,
 };
 
