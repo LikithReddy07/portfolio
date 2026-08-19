da4edb724b1580dcf6aa29880d990ebbd0e1d4d6
Likith Reddy S <likithreddy@Mac-1.lan>
2026-08-20 02:58:59 +0530

    removed pdf url

--- src/config/data.ts
+++ src/config/data.ts
@@ -14,7 +14,7 @@ export const personalInfo = {
 export const cvConfig = {
   enabledInHero: false,
   enabledInNavbar: false,
-  url: "https://github.com/LikithReddy07/portfolio/blob/0868c3cf567adb733ded819fede34121b3ce82b3/public/Likith_CV_V2.pdf", // replace with your actual CV PDF URL
+  url: "", // replace with your actual CV PDF URL
   forceDownload: true,
 };
 
